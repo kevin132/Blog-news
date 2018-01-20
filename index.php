fe9d7fd2bde0929a3baf079e0a57eae0960adba9
kevin132 <chenkevin@hotmail.fr>
2018-01-20 01:52:35 +0100

    end index

--- index.php
+++ index.php
@@ -10,37 +10,41 @@
 
 </head>
 <body class="index-body">
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <?php require 'partials/header.php'; ?>
+        <?php require 'partials/header.php'; ?>
 
-    <div class="row my-3 index-content">
+        <div class="row my-3 index-content">
 
-        <?php require 'partials/nav.php'; ?>
+            <?php require 'partials/nav.php'; ?>
 
-        <main class="col-9">
-            <section class="latest_articles">
-                <header class="mb-4"><h1>Les 3 derniers articles :</h1></header>
+            <main class="col-9">
+                <section class="latest_articles">
+                    <header class="mb-4"><h1>Les 3 derniers articles :</h1></header>
 
-                <?php
+                    <?php
 
-                $response = $bdd->query('SELECT * FROM article WHERE id= 6');
-                while ($donnees = $response->fetch()) {
-                    echo '<h2 class="article-title">' . $donnees['title'] . '</h2>';
+                    $query = $bdd->query('SELECT * FROM article WHERE is_published = 1 ORDER BY created_at DESC LIMIT 0, 3');
 
-                }
+                    while ($data = $query->fetch()) {
 
-                ?>
+                        echo '<h2 class="article-title">' . $data['title'] . '</h2>';
 
-            </section>
-            <div class="text-right">
-                <a href="article_list.php">> Tous les articles</a>
-            </div>
-        </main>
+                        echo'<p> '.'Créé le '. $data['created_at'].'<br>'.$data['summary'].'</p>';
+
+                    }
+
+                    ?>
+
+                </section>
+                <div class="text-right">
+                    <a href="article_list.php"> Tous les articles</a>
+                </div>
+            </main>
+        </div>
+
+        <?php require 'partials/footer.php'; ?>
+
     </div>
-
-    <?php require 'partials/footer.php'; ?>
-
-</div>
 </body>
 </html>
